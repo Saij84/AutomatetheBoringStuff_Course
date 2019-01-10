ab7ed88283fccc5afc1c7fe7673f7944db31c432
fangc <saij84@gmail.com>
2019-01-10 18:49:49 +1100

    adding toDo list to program

---
+++ src/scripts/12_guessNumber.py
@@ -0,0 +1,13 @@
+#toDo
+'''
+-player need to guess a random number between 1 - 20
+    -generate random number between 1-20
+-player only have 6 guesses
+-player need to be able to input a number
+    -check need to be performed to ensure number us between 1 - 20
+    -check that player is inputing int
+-after input give player hint
+    -"Your guess is too high"
+    -"Your guess is too low"
+    -"You guessed right, the number i thinking of is {number}
+'''
