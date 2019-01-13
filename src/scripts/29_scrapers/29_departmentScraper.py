e4c16506acf7097bb1daf5c576e631f9c70a7dd6
fangc <saij84@gmail.com>
2019-01-13 21:16:05 +1100

    extract email regular expressions

--- src/scripts/29_scrapers/29_departmentScraper.py
+++ src/scripts/29_scrapers/29_departmentScraper.py
@@ -4,6 +4,32 @@ scraping raw data for department
 
 import re
 
-re.compile('''
+rawDataPath = "C:\\tools\\AutomatetheBoringStuff_Course\\src\scripts\\29_scrapers\\rawData\\"
+#file = "singelData.txt"
+file = "rawContactData.txt"
 
-''', re.VERBOSE)
+depIDRegex = re.compile("""
+[a-z0-9@.!#$%&'*+/=?^_`{|}~-]*@[a-z0-9@.!#$%&'*+/=?^_`{|}~-]+[au|com|net]
+""", re.VERBOSE | re.DOTALL)
+
+# roleRegex = re.compile("""
+# (\d{6,7}\s\w?-\d{5,6})
+# """, re.VERBOSE)
+#
+# emailRegex = re.compile("""
+# (\d{6,7}\s\w?-\d{5,6})
+# """, re.VERBOSE)
+#
+# phoneRegex = re.compile("""
+# (\d{6,7}\s\w?-\d{5,6})
+# """, re.VERBOSE)
+
+rawFileData = open(rawDataPath+file, mode="r")
+fileData = rawFileData.read()
+
+depID = depIDRegex.findall(fileData)
+
+print(depID)
+print(len(depID))
+
+rawFileData.close()
